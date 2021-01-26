6ed63a442732d276c89e29b8db70a0656efc57e9
Ankcorn <thomasankcorn@gmail.com>
2021-01-26 21:10:23 +0000

    arc-progressive-enhanced-form

---
+++ app.arc
@@ -0,0 +1,16 @@
+@app
+arc-prog
+
+@http
+get /
+post /api/emails
+
+
+@tables
+accounts
+  email *String
+
+# @aws
+# profile default
+# region us-west-1
+  
---
+++ src/http/get-index/index.js
@@ -0,0 +1,17 @@
+const eta = require('eta')
+const arc = require('@architect/functions');
+
+exports.handler = async function http (req) {
+  const data = { answer: 43, css: arc.static('./compiled.css') };
+  eta.configure({
+    views: __dirname
+  });
+  return {
+    statusCode: 200,
+    headers: {
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
+      'content-type': 'text/html; charset=utf8'
+    },
+    body: await eta.renderFile('./main', data)
+  }
+}
