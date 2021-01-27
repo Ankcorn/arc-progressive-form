1364e686c18c9cd6c6b8d58550d49434b679e0f7
Ankcorn <thomasankcorn@gmail.com>
2021-01-27 09:55:42 +0000

    add button logic

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -12,6 +12,6 @@ exports.handler = async function http (req) {
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
       'content-type': 'text/html; charset=utf8'
     },
-    body: await eta.renderFile('./main', data)
+    body: await eta.renderFile('index', data)
   }
 }
